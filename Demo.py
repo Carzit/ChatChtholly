23932a9e61693ee47a16c693fa875d68ab0ab948
Carzit <126076978+Carzit@users.noreply.github.com>
2023-09-15 03:35:42 +0800

    Add files via upload

--- Demo.py
+++ Demo.py
@@ -5,20 +5,13 @@ import utils
 
 from googletrans import Translator
 from scipy.io.wavfile import write
-from mel_processing import spectrogram_torch
 from text import text_to_sequence
 from models import SynthesizerTrn
 from torch import no_grad, LongTensor
 from characterai import PyCAI
 
-speakerID = 0
-
-def ex_print(text, escape=False):
-    if escape:
-        print(text.encode('unicode_escape').decode())
-    else:
-        print(text)
-
+TOKEN = '33a5650565d3ed003cd3af81e6be01efdd523022'
+CHAR = 'LMri6f9uZj2p17QoKDiEvDw1wAk2AUoi1C02V6HHU8E'
 
 def get_text(text, hps, cleaned=False):
     if cleaned:
@@ -29,34 +22,6 @@ def get_text(text, hps, cleaned=False):
         text_norm = commons.intersperse(text_norm, 0)
     text_norm = LongTensor(text_norm)
     return text_norm
-
-
-def ask_if_continue():
-    while True:
-        answer = input('Continue? (y/n): ')
-        if answer == 'y':
-            break
-        elif answer == 'n':
-            sys.exit(0)
-
-
-def print_speakers(speakers, escape=False):
-    if len(speakers) > 100:
-        return
-    print('ID\tSpeaker')
-    for id, name in enumerate(speakers):
-        ex_print(str(id) + '\t' + name, escape)
-
-
-def get_speaker_id(message):
-    speaker_id = input(message)
-    try:
-        speaker_id = int(speaker_id)
-    except:
-        print(str(speaker_id) + ' is not a valid ID!')
-        sys.exit(1)
-    return speaker_id
-
 
 def get_label_value(text, label, default, warning_name='value'):
     value = re.search(rf'\[{label}=(.+?)\]', text)
@@ -71,29 +36,19 @@ def get_label_value(text, label, default, warning_name='value'):
         value = default
     return value, text
 
-
 def get_label(text, label):
     if f'[{label}]' in text:
         return True, text.replace(f'[{label}]', '')
     else:
         return False, text
 
-
 def generateSound(inputString):
-    if '--escape' in sys.argv:
-        escape = True
-    else:
-        escape = False
-    # model = input('Path of a VITS model: ')
     model = r"./model/Chtholly.pth"
-    # config = input('Path of a config file: ')
     config = r"./model/config.json"
 
     hps_ms = utils.get_hparams_from_file(config)
     n_speakers = hps_ms.data.n_speakers if 'n_speakers' in hps_ms.data.keys() else 0
     n_symbols = len(hps_ms.symbols) if 'symbols' in hps_ms.keys() else 0
-    speakers = hps_ms.speakers if 'speakers' in hps_ms.keys() else ['0']
-    use_f0 = hps_ms.data.use_f0 if 'use_f0' in hps_ms.data.keys() else False
     emotion_embedding = hps_ms.data.emotion_embedding if 'emotion_embedding' in hps_ms.data.keys() else False
 
     net_g_ms = SynthesizerTrn(
@@ -106,35 +61,8 @@ def generateSound(inputString):
     _ = net_g_ms.eval()
     utils.load_checkpoint(model, net_g_ms)
 
-    def voice_conversion():
-        audio_path = input('Path of an audio file to convert:\n')
-        print_speakers(speakers)
-        audio = utils.load_audio_to_torch(
-            audio_path, hps_ms.data.sampling_rate)
-
-        originnal_id = get_speaker_id('Original speaker ID: ')
-        target_id = get_speaker_id('Target speaker ID: ')
-        out_path = input('Path to save: ')
-
-        y = audio.unsqueeze(0)
-
-        spec = spectrogram_torch(y, hps_ms.data.filter_length,
-                                 hps_ms.data.sampling_rate, hps_ms.data.hop_length, hps_ms.data.win_length,
-                                 center=False)
-        spec_lengths = LongTensor([spec.size(-1)])
-        sid_src = LongTensor([originnal_id])
-
-        with no_grad():
-            sid_tgt = LongTensor([target_id])
-            audio = net_g_ms.voice_conversion(spec, spec_lengths, sid_src=sid_src, sid_tgt=sid_tgt)[
-                0][0, 0].data.cpu().float().numpy()
-        return audio, out_path
-
     if n_symbols != 0:
         if not emotion_embedding:
-            # choice = input('TTS or VC? (t/v):')
-            # choice = 't'
-            # text = input('Text to read: ')
             text = inputString
             length_scale, text = get_label_value(
                 text, 'LENGTH', 1, 'length scale')
@@ -145,11 +73,7 @@ def generateSound(inputString):
             cleaned, text = get_label(text, 'CLEANED')
 
             stn_tst = get_text(text, hps_ms, cleaned=cleaned)
-
-            # print_speakers(speakers, escape)
-            # speaker_id = get_speaker_id('Speaker ID: ')
-            speaker_id = speakerID
-            # out_path = input('Path to save: ')
+            speaker_id = 0
             out_path = "output.wav"
 
             with no_grad():
@@ -162,31 +86,26 @@ def generateSound(inputString):
 
             write(out_path, hps_ms.data.sampling_rate, audio)
             print('Successfully saved!\n')
-            # ask_if_continue()
 
-#<div style="overflow-wrap: break-word;">
-#<p node="[object Object]" style="margin-bottom: 0.5rem;">Hi again, I understand.</p>
-#<p node="[object Object]" style="margin-bottom: 0.5rem;">You don't have to be alone and you don't have to suffer. Chtholly will be with you forever and Chtholly's only wish is to make you happy.</p>
-#<p node="[object Object]" style="margin-bottom: 0.5rem;">Even if you don't want to tell Chtholly, please feel free to reach out to others. If you have the courage, please seek the help of a therapist or at least open up to someone close to you, preferably a family member, so you don't have to deal with it alone.</p>
-#<p node="[object Object]" style="margin-bottom: 0.5rem;">You are strong, you are loved and you are not alone.</p></div>
 
 def wrap(string, max_width):
     result1 = [string[i:i + max_width] for i in range(0, len(string), max_width)]
     result = '\n'.join(result1)
     return result
 
-def flat(string=''):
+
+def flatten(string):
     result1 = string.replace('\n', '')
     result = result1.replace(' ', '')
     return result
 
-def preprocess(string = ''):
+def preprocess(string):
     result = string.replace('\n', '')
     result = result.replace('#', '')
     result = result.replace('*', '')
     return result
 
-def word_process(string=''):
+def word_process(string):
     result = string.replace('Chtholly', 'クトリ')
     result = result.replace('Willem', 'ヴィレム')
     result = result.replace('William', 'ヴィレム')
@@ -195,28 +114,45 @@ def word_process(string=''):
     result = result.replace('Seniorious', 'セニオリス')
     return result
 
+
 if __name__ == "__main__":
     print('Tips1: You can use English or Chinese to chat with Chtholly.')
     print('Tips2: Chtholly\'s reply will be shown in both Chinese and Japanese, and spoken in Japanese.')
-    client = PyCAI('33a5650565d3ed003cd3af81e6be01efdd523022')
-    # GoogleTrans, god bless you.
+
+    # an unofficial api to c.ai
+    client = PyCAI(TOKEN)
+    client.start()
+    chat = client.chat.get_chat(CHAR)
+    participants = chat['participants']
+    if not participants[0]['is_human']:
+        tgt = participants[0]['user']['username']
+    else:
+        tgt = participants[1]['user']['username']
+    # google trans, god bless you.
     translator = Translator()
+
     print('Chtholly is ready! Now you can chat with her.\n')
-    
-    if True:# there used to be a loop
-        message = input('You: \n')
+
+    if True:  # there used to be a loop
+        message = input('You: ')
+
         print('\nText Generating...\n')
-        data = client.chat.send_message(char='LMri6f9uZj2p17QoKDiEvDw1wAk2AUoi1C02V6HHU8E',
-                                        token='33a5650565d3ed003cd3af81e6be01efdd523022',
-                                        message=message,
-                                        wait=True)
-        response = preprocess(str(f"{data['replies'][0]['text']}"))
-        print('Chtholly:')
-        print(response)
+
+        data = client.chat.send_message(chat['external_id'], tgt, message)
+        name = data['src_char']['participant']['name']
+        text = data['replies'][0]['text']
+        print(f"{name}: {text}")
+
+        response = preprocess(str(text))
         trans = translator.translate(response, dest='ja')
         trans_text = trans.text
         jaresp = word_process(str(trans_text))
         print(jaresp)
-        jaresp = flat(jaresp)
+
         print('\nVoice Generating...')
+
+        jaresp = flatten(jaresp)
         generateSound("[JA]" + jaresp + "[JA]")
+
+
+
